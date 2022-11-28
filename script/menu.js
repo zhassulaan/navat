37869751015c62e9bea35dad9d34288d30d75edd
Zhassulan <190103383@stu.sdu.edu.kz>
2022-11-28 12:32:34 +0600

    final changes

--- script/menu.js
+++ script/menu.js
@@ -1,16 +1,20 @@
 let header = document.getElementById("menu");
 
 function openNavbar() {
-	header.style.left = "0";
+	header.style.top = "0";
+	header.style.height = "auto";
+	header.style.opacity = "1";
 	header.style.transition = "all 1.5s ease-in-out";
 }
  
 function closeNavbar() {
-	header.style.left = "-100%";
+	header.style.top = "-100%";
+	header.style.opacity = "0"
 	header.style.transition = "all 1.5s ease-in-out";
 }
  
 function clickLink() {
-	header.style.left = "-100%";
+	header.style.top = "-100%";
+	header.style.opacity = "0"
 	header.style.transition = "all 0.75s ease-in-out";
 }
