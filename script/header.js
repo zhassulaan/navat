f3f2dd4fb0fa026a6d16941572da881d0366a608
Zhassulan <190103383@stu.sdu.edu.kz>
2022-11-29 17:49:54 +0600

    locations block replaced

--- script/header.js
+++ script/header.js
@@ -8,15 +8,23 @@ $(element).scroll(function() {
 		$(navbar).removeClass("navbar3");
 		$(navbar).removeClass("navbar4");
 		$(navbar).addClass("navbar1");
-	} else if ($(element).scrollTop() < 2 * height - 1) {
+	} else if ($(element).scrollTop() < 2 * height) {
 		$(navbar).removeClass("navbar1");
 		$(navbar).removeClass("navbar3");
 		$(navbar).addClass("navbar4");
-	} else if ($(element).scrollTop() < 4 * height + menuHeight.clientHeight - 1) {
+	} else if ($(element).scrollTop() < 3 * height + menuHeight.clientHeight) {
 		$(navbar).removeClass("navbar1");
 		$(navbar).removeClass("navbar4");
 		$(navbar).addClass("navbar3");
-	} else if ($(element).scrollTop() < 6 * height + menuHeight.clientHeight - 1) {
+	} else if ($(element).scrollTop() < 4 * height + menuHeight.clientHeight) {
+		$(navbar).removeClass("navbar1");
+		$(navbar).removeClass("navbar3");
+		$(navbar).addClass("navbar4");
+	} else if ($(element).scrollTop() < 5 * height + menuHeight.clientHeight) {
+		$(navbar).removeClass("navbar1");
+		$(navbar).removeClass("navbar4");
+		$(navbar).addClass("navbar3");
+	} else if ($(element).scrollTop() < 6 * height + menuHeight.clientHeight) {
 		$(navbar).removeClass("navbar1");
 		$(navbar).removeClass("navbar3");
 		$(navbar).addClass("navbar4");
