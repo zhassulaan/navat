9d168af493b3e47e5f11e2449454dd2e88051e89
Zhassulan <190103383@stu.sdu.edu.kz>
2022-11-30 15:28:46 +0600

    scroll effect

--- script/scroll.js
+++ script/scroll.js
@@ -1,41 +1,40 @@
 var pageHeight = window.innerHeight;
 var isAnimating = false;
-document.body.style.transform = 'translate3d(0px,0px,0px)';
+document.body.style.transform = 'translate3d(0px, 0px, 0px)';
 
-document.addEventListener('scroll', function(e){
+document.addEventListener('scroll', function(e) {
   document.body.scrollTop = 0;
 });
 document.addEventListener('wheel', wheelListener);
 
 function wheelListener(e) {
-  if(e.deltaY > 0) {
-    scrollPage(-pageHeight);
-  } else {
-    scrollPage(+pageHeight);
-  }
+  	if (e.deltaY > 0)
+    	scrollPage(-pageHeight);
+  	else
+    	scrollPage(+pageHeight);
 }
 
 function scrollPage(scrollSize) {
-  if(isAnimating){
-    return;
-  }
-  isAnimating = true;
-  var yPos = getNewYPos(scrollSize);
-  document.body.style.transform = 'translate3d(0px,'+ yPos + ',0px)';
+  	if (isAnimating)
+    	return;
+  
+	isAnimating = true;
+	var yPos = getNewYPos(scrollSize);
+	document.body.style.transform = 'translate3d(0px,' + yPos + ',0px)';
 }
 
-function getNewYPos(add){
-  var oldYPos = document.body.style.transform.split(',')[1];
-  oldYPos = parseInt(oldYPos.replace(/px/,''));
-  var newYPos = oldYPos + add;
-  if(newYPos > 0){
-    isAnimating = false;
-  }
-  return Math.min(0, newYPos) + 'px';
+function getNewYPos(add) {
+	var oldYPos = document.body.style.transform.split(',')[1];
+	oldYPos = parseInt(oldYPos.replace(/px/,''));
+	var newYPos = oldYPos + add;
+	if (newYPos > 0)
+		isAnimating = false;
+
+	return Math.min(0, newYPos) + 'px';
 }
 
 
-document.body.addEventListener('transitionend', function(){
-  setTimeout(function(){ isAnimating = false; }, 500);
-  document.addEventListener('wheel', wheelListener);
-})
+document.body.addEventListener('transitionend', function() {
+	setTimeout(function() { isAnimating = false; }, 100);
+	document.addEventListener('wheel', wheelListener);
+});
